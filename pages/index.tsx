6a139e6c1f3228a95fb3b29d38b3d43b9fe5887f
kyungho <epsoe@naver.com>
2022-02-05 22:07:04 +0900

    Modifier More

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
           </button>
         </div>
       </div>
-      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl group">
         <div className="bg-blue-500 p-6 pb-14">
           <span className="text-white text-2xl">Profile</span>
         </div>
@@ -43,7 +43,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-blue-500 transition-colors" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
@@ -90,6 +90,37 @@ const Home: NextPage = () => {
             </button>
           </div>
         </div>
+      </div>
+      <form className="flex flex-col space-y-2 bg-blue-500 p-5">
+        <input
+          type="text"
+          required
+          placeholder="Username"
+          className="border p-1 peer border-gray-400 rounded-sm"
+        />
+        <span className="hidden peer-invalid:block peer-invalid:text-red-500">
+          This input is invalid
+        </span>
+        <span className="hidden peer-valid:text-teal-500 peer-valid:block">
+          Awesome Username
+        </span>
+        <input type="submit" value="Login" className="bg-white" />
+      </form>
+      <div className="flex flex-col space-y-2 bg-blue-500 p-5">
+        <details className="select-none open:text-white open:bg-indigo-500">
+          <summary className="cursor-pointer">What is my fav. food</summary>
+          <span>김치</span>
+        </details>
+        <ul className="list-decimal marker:text-teal-500">
+          <li>hi</li>
+          <li>hi</li>
+          <li>hi</li>
+          <li>hi</li>
+        </ul>
+        <input
+          type="file"
+          className="file:border-0 file:bg-purple-400 file:rounded-md "
+        />
       </div>
     </div>
   );
