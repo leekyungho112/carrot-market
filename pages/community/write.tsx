43d3fd26eef9c246e9d10b5f2d352b2588cd21b5
kyungho <epsoe@naver.com>
2022-02-10 17:24:20 +0900

    TextArea Refactoring

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,19 +1,20 @@
 import { NextPage } from 'next';
+import Button from '../../components/button';
 import Layout from '../../components/layout';
+import Textarea from '../../components/textarea';
 
 const Write: NextPage = () => {
   return (
-    <Layout canGoBack>
+    <Layout canGoBack title="Write Post">
       {' '}
-      <form className="px-4 py-10">
-        <textarea
-          className="mt-1 shadow-sm w-full  focus:ring-orange-500  rounded-md border-gray-300 focus:border-orange-500"
-          rows={4}
+      <form className="px-4 space-y-4">
+        <Textarea
+          label="Post"
+          name="post"
+          required
           placeholder="Ask a question!"
         />
-        <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:outline-none focus:ring-orange-500">
-          Submit
-        </button>
+        <Button text="Submit" />
       </form>
     </Layout>
   );
--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -1,66 +1,26 @@
 import { NextPage } from 'next';
+import Button from '../../components/button';
+import Input from '../../components/input';
 import Layout from '../../components/layout';
+import Textarea from '../../components/textarea';
 
 const Create: NextPage = () => {
   return (
-    <Layout canGoBack>
-      <div className="py-10 space-y-5 px-4">
-        <div>
-          <label
-            className="mb-1 block text-sm font-medium text-gray-700"
-            htmlFor="name"
-          >
-            Name
-          </label>
-          <div className="rounded-md relative flex  items-center shadow-sm">
-            <input
-              id="name"
-              type="email"
-              className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-              required
-            />
-          </div>
-        </div>
-        <div>
-          <label
-            className="mb-1 block text-sm font-medium text-gray-700"
-            htmlFor="price"
-          >
-            Price
-          </label>
-          <div className=" rounded-md flex items-center shadow-md relative">
-            <div className="absolute left-0 pl-3 pointer-events-none flex items-center justify-center">
-              <span className="text-gray-700 text-sm">$</span>
-            </div>
-            <input
-              className=" appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-              id="price"
-              type="text"
-              placeholder="0.00"
-            />
-            <div className="absolute pointer-events-none right-0 pr-3 flex items-center">
-              <span className="text-gray-500">USD</span>
-            </div>
-          </div>
-        </div>
-        <div>
-          <label
-            htmlFor="description"
-            className="mb-1 block text-sm font-medium text-gray-700"
-          >
-            Description
-          </label>
+    <Layout canGoBack title="Go Live">
+      <form className="py-10 space-y-4 px-4">
+        <Input label="Name" required name="name" type="text" />
+        <Input
+          label="Price"
+          required
+          name="price"
+          type="text"
+          placeholder="0.00"
+          kind="price"
+        />
+        <Textarea name="description" label="Description" />
 
-          <textarea
-            id="description"
-            className="mt-1 shadow-sm w-full  focus:ring-orange-500  rounded-md border-gray-300 focus:border-orange-500"
-            rows={4}
-          />
-        </div>
-        <button className="mt-5 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:outline-none focus:ring-orange-500">
-          Upload product
-        </button>
-      </div>
+        <Button text="Go Live" />
+      </form>
     </Layout>
   );
 };
