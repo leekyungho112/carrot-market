603973cc4ebe254ec2082c34bbbee3a897b9de2b
kyungho <epsoe@naver.com>
2022-02-16 12:47:53 +0900

    Validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,23 +1,27 @@
 import { useForm } from 'react-hook-form';
 
 export default function Forms() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit } = useForm();
 
+  const onValid = () => {
+    console.log('im valid bby');
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid)}>
       <input
         type="text"
-        {...register('username')}
+        {...(register('username'), { required: true })}
         placeholder="username"
-        required
       />
-      <input {...register('email')} type="email" placeholder="Email" required />
       <input
-        {...register('password')}
+        {...(register('email'), { required: true })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...(register('password'), { required: true })}
         type="password"
         placeholder="Password"
-        required
       />
 
       <input type="submit" value="Create" />
