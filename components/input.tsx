5849070f132c7520f39ded849dad901f9cb21059
kyungho <epsoe@naver.com>
2022-02-16 17:13:50 +0900

    Enter Form

--- components/input.tsx
+++ components/input.tsx
@@ -1,11 +1,22 @@
+import type { UseFormRegisterReturn } from 'react-hook-form';
+
 interface InputProps {
   label: string;
   name: string;
   kind?: 'text' | 'phone' | 'price';
-  [key: string]: any;
+  register: UseFormRegisterReturn;
+  type: string;
+  required: boolean;
 }
 
-const Input = ({ label, name, kind = 'text', ...rest }: InputProps) => {
+const Input = ({
+  label,
+  name,
+  kind = 'text',
+  register,
+  type,
+  required,
+}: InputProps) => {
   return (
     <div>
       <label
@@ -18,7 +29,9 @@ const Input = ({ label, name, kind = 'text', ...rest }: InputProps) => {
         <div className="rounded-md relative flex  items-center shadow-sm">
           <input
             id={name}
-            {...rest}
+            {...register}
+            type={type}
+            required={required}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
         </div>
@@ -30,7 +43,9 @@ const Input = ({ label, name, kind = 'text', ...rest }: InputProps) => {
           </div>
           <input
             id={name}
-            {...rest}
+            {...register}
+            type={type}
+            required={required}
             className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
           <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -45,7 +60,9 @@ const Input = ({ label, name, kind = 'text', ...rest }: InputProps) => {
           </span>
           <input
             id={name}
-            {...rest}
+            {...register}
+            type={type}
+            required={required}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
         </div>
