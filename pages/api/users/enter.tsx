ae13ed30f3d802c995dbfd06d6b642c6f74758eb
kyungho <epsoe@naver.com>
2022-02-18 12:40:24 +0900

    Account Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,8 +3,57 @@ import client from '@libs/server/client';
 import withHandler from '@libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anony',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+  /* if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log('존재하는 유저입니다.');
+    if (!user) {
+      console.log('유저를 찾을수 없습니다.');
+      user = await client.user.create({
+        data: {
+          name: 'Anony',
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log('존재하는 유저입니다.');
+    if (!user) {
+      console.log('유저를 찾을수 없습니다.');
+      user = await client.user.create({
+        data: {
+          name: 'Anony',
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  } */
+
+  return res.status(200).end();
 }
 
 export default withHandler('POST', handler);
