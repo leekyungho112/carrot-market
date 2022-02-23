9ec1192b549db7015813ec58a0ee428f07fcfd31
kyungho <epsoe@naver.com>
2022-02-23 12:03:52 +0900

    useUser Refactor

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,13 +1,16 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import useSWR from 'swr';
 
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
-
 export default function useUser() {
-  const { data, error } = useSWR('/api/users/me', fetcher);
+  const { data, error } = useSWR('/api/users/me');
+  const router = useRouter();
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.push('/enter');
+    }
+  }, [data, router]);
 
-  // return router.push('/enter');
-
-  return data;
+  return { user: data?.profile, isLoading: !data && !error };
 }
